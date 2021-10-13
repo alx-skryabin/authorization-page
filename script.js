3606e00c136a0ab3fcd4c593241cc1d3af3ab996
A.Skriabin <A.Skriabin@asntl.ru>
2021-10-13 16:03:25 +0300

    Formatting code. Add README.md

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-
 (function () {
-	$('.auth-container_form').on('submit', (e) => {
-		e.preventDefault();
-		let form = e.target;
-		let typeForm = form.getAttribute('data-type'); 
-		let dataForm = {};
+  $('.auth-container_form').on('submit', (e) => {
+    e.preventDefault();
+    let form = e.target;
+    let typeForm = form.getAttribute('data-type');
+    let dataForm = {};
 
-		if (typeForm === 'auth') {
-			dataForm = {
-				type: typeForm,
-				login: form[0].value,
-				pass: form[1].value
-			};
-		}
-		else {
-			dataForm = {
-				type: typeForm,
-				mail: form[0].value
-			};
-		}
+    if (typeForm === 'auth') {
+      dataForm = {
+        type: typeForm,
+        login: form[0].value,
+        pass: form[1].value
+      };
+    } else {
+      dataForm = {
+        type: typeForm,
+        mail: form[0].value
+      };
+    }
 
-		console.log("dataForm", dataForm);
-	});
+    console.log("dataForm", dataForm);
+  });
 })()
